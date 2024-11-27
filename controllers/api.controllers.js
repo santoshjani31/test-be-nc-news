4bd93e8b11b5fd32101b7bf82d3afcaf92fb3cf5
santoshjani31 <santosh.jani@gmail.com>
2024-11-27 13:44:19 +0000

    add get /api/topics endpoint

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -1,5 +1,12 @@
 const endpointsJson = require('../endpoints.json');
+const { fetchApiTopics } = require('../models/api.models.');
 
 exports.getApi = (req, res) => {
   res.status(200).send({ endpoints: endpointsJson });
+};
+
+exports.getApiTopics = (req, res) => {
+  fetchApiTopics().then((topics) => {
+    res.status(200).send({ topics });
+  });
 };
